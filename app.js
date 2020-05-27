05f3ec13470d8512c27654acf6149bcc755ab8b2
DanB-Web <dan.bayford@gmail.com>
2020-05-27 09:12:15 +0100

    Routing
    Set up dynamic routing for project pages and created 404 Pug template

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
+//Load dependencies
 const express = require("express");
-const data = require("./data.json");
+const { projects } = require("./data.json");
 
+//Instantiate instance of Express
 const app = express();
 
 //const router = express.Router();
 
-
-// view engine setup
+// View engine setup
 app.set('view engine', 'pug');
 
-//Allow Express to serve static assets in 'public' folder
+//Allow Express to serve static assets from 'public' folder
 app.use(express.static('public'));
 
-
-
-app.get('/', (req, res) => {   
-      res.render('index', { data });  
+//Routes
+app.get('/', (req, res, next) => {   
+      res.render('index', { projects });  
       console.log("index GET")
 });
+
+app.get('/about', (req, res, next) => {   
+    res.render('about', { projects });  
+    console.log("route GET")
+});
+
+app.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    console.log(`route Project ${projectId} GET`);
+    if (project) {
+      res.render('project', { projects });
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
+//Error Handling
+app.use(function (req, res, next) {
+    res.status(404).render('nope');
+    console.log("This page doesn't exist ya wally!");
+  })
 
 //Local development server
 app.listen(3000, () => {
